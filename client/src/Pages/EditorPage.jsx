11cc92620477a768ab6cd630e0ec3d697371d862
Davinder Kumar <abhiverma97819@gmail.com>
2023-08-16 23:17:01 +0530

    working on some bugs

--- client/src/Pages/EditorPage.jsx
+++ client/src/Pages/EditorPage.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import Client from "../Components/Client";
+import Editor from "../Components/Editor";
 
 const EditorPage = () => {
-  return <div>Editor</div>;
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Rakesh K" },
+    { socketId: 2, username: "Rakesh K" },
+    { socketId: 3, username: "John K" },
+  ]);
+
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/code-sync.png" alt="logo" />
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+        <button
+          className="btn copyBtn"
+          // onClick={copyRoomId}
+        >
+          Copy ROOM ID
+        </button>
+        <button
+          className="btn leaveBtn"
+          // onClick={leaveRoom}
+        >
+          Leave
+        </button>
+      </div>
+      <div className="editorWrap">
+        <Editor />
+        {/* <Editor
+          socketRef={socketRef}
+          roomId={roomId}
+          onCodeChange={(code) => {
+            codeRef.current = code;
+          }}
+        /> */}
+      </div>
+    </div>
+  );
 };
 
 export default EditorPage;
--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,11 +1,44 @@
 import React, { useState } from "react";
+import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
 
 const HomePage = () => {
   const navigate = useNavigate();
 
   const [roomId, setRoomId] = useState("");
   const [username, setUsername] = useState("");
+
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+    toast.success("Created a new room");
+  };
+
+  const joinRoom = () => {
+    if (!roomId) {
+      toast.error("ROOM ID is required");
+      return;
+    }
+    if (!username) {
+      toast.error("Username is required");
+      return;
+    }
+
+    // Redirect
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username,
+      },
+    });
+  };
+
+  const handleInputEnter = (e) => {
+    if (e.code === "Enter") {
+      joinRoom();
+    }
+  };
 
   return (
     <div className="homePageWrapper">
@@ -33,19 +66,12 @@ const HomePage = () => {
             value={username}
             onKeyUp={handleInputEnter}
           />
-          <button
-            className="btn joinBtn"
-            // onClick={joinRoom}
-          >
+          <button className="btn joinBtn" onClick={joinRoom}>
             Join
           </button>
           <span className="createInfo">
             If you don't have an invite then create &nbsp;
-            <a
-              // onClick={createNewRoom}
-              href=""
-              className="createNewBtn"
-            >
+            <a onClick={createNewRoom} href="" className="createNewBtn">
               new room
             </a>
           </span>
@@ -54,7 +80,7 @@ const HomePage = () => {
       <footer>
         <h4>
           Built with 💛 &nbsp; by &nbsp;
-          <a href="https://github.com/codersgyan">Coder's Gyan</a>
+          <a href="https://github.com/abhi07070">Davinder Kumar</a>
         </h4>
       </footer>
     </div>
