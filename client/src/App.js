93a1e80a5f221832d7173171dee8c180a8cf99a4
Davinder Kumar <abhiverma97819@gmail.com>
2023-08-16 22:01:46 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import EditorPage from "./Pages/EditorPage";
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/editor/:roomId" element={<EditorPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
